2be787a4247e0562d8f0ebcb714df66a31c89047
CPYu5239 <57282801+CPYu5239@users.noreply.github.com>
2019-12-24 17:31:48 +0800

    完成腳本練習

---
+++ UnityProject/Assets/Script/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [Header("血量")]
+    public int hp = 100;
+    [Header("攻擊")]
+    public int attack;
+    [Header("殭屍")]
+    public Zombie zombie;
+    [Header("音源")]
+    public AudioSource aud;
+    [Header("音效")]
+    public AudioClip clip;
+
+    private void Update()
+    {
+        Attack();
+        
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            attack = Random.Range(5, 20);
+            zombie.hp -= attack;
+            aud.PlayOneShot(clip, 1);
+            print("<color=green>殭屍受到" + attack + "傷害。</color>");
+            print("<color=green>殭屍剩餘血量 : " + zombie.hp + "</color>");
+            if (zombie.hp < 0)
+            {
+                print("<color=green>殭屍死亡</color>");
+            }
+        }
+    }
+}
---
+++ UnityProject/Assets/Script/Zombie.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    [Header("血量")]
+    public int hp = 100;
+    [Header("攻擊")]
+    public int attack;
+    [Header("玩家")]
+    public Player player;
+    [Header("音源")]
+    public AudioSource aud;
+    [Header("音效")]
+    public AudioClip clip;
+
+    private void Update()
+    {
+        Attack();
+        
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            attack = Random.Range(5, 20);
+            player.hp -= attack;
+            aud.PlayOneShot(clip, 1);
+            print("<color=red>玩家受到" + attack + "傷害。</color>");
+            print("<color=red>玩家剩餘血量 : " + player.hp + "</color>");
+            if (player.hp <= 0)
+            {
+                print("<color=red>玩家死亡</color>");
+            }
+        }
+    }
+}
